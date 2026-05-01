9d3635149114fc738055c7ff293f5c4a79bdc871
Vitaly Kashavkin <kashavkin_v.v@mail.ru>
2026-05-01 13:04:31 +0500

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Vitaly Kashavkin on 01.05.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
